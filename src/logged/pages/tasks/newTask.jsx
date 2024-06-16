2c9fe53abeb9128725f7cad9ca2dcfe542146254
JoaoVictorPires <61362665+JoaoVictorPires@users.noreply.github.com>
2024-06-16 17:43:14 -0300

    chore: adjust padding

--- src/logged/pages/tasks/newTask.jsx
+++ src/logged/pages/tasks/newTask.jsx
@@ -10,24 +10,24 @@ const newTask = () => {
 
     return (
 
-        <div>
-
+        <div  className='p-4'>
+ 
             <h1>Nova Tarefa</h1>
 
             <form onSubmit={handleSubmited}>
 
-                <div className="form-group">
+                <div className="form-group p-2">
                     <label htmlFor="title">Título</label>
                     <input type="text" className="form-control" id="title" placeholder='Colher trigo...' required />
                 </div>
-                <div className="form-group">
+                <div className="form-group p-2">
                     <label htmlFor="description">Descrição</label>
                     <textarea className="form-control" id="description" rows="3" placeholder='Colher trigo na aldeia mais próxima e guardar em meu estoque...' required></textarea>
                 </div>
-                <div className='p-1 d-flex justify-content-center ml-1'>
+                <div className='p-2 d-flex justify-content-center gap-2'>
                     <button type="submit" className="btn btn-success">Salvar</button>
 
-                    <button className="btn btn-danger" onClick={() => navigate('/')}>Voltar</button>
+                    <button className="btn btn-danger  " onClick={() => navigate('/')}>Voltar</button>
                 </div>
 
             </form>
